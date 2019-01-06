838e0b9062b0393070f4760920c320cc78010a31
udilugasi <30871653+udilugasi@users.noreply.github.com>
2019-01-06 03:39:46 +0200

    Update aws_instances.sh

--- aws_instances.sh
+++ aws_instances.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# read aws account details from the user
+read -p "Enter your aws_access_key_id and press [ENTER]: " access_key
+read -p "Enter your aws_secret_access_key and press [ENTER]: " secret_access_key
+read -p "Enter your aws_region and press [ENTER]: " region
+
 ####Installing AWS CLI with all dependencies.
 wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
 unzip awscli-bundle.zip
@@ -8,9 +13,9 @@ sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 ./awscli-bundle/install -h
 
 ###Configure the config and credentials with your account.
-aws configure set aws_access_key_id **********
-aws configure set aws_secret_access_key ***************
-aws configure set default.region us-east-1
+aws configure set aws_access_key_id $access_key
+aws configure set aws_secret_access_key $secret_access_key
+aws configure set default.region $region
 
 ###Get all instances details in table format
-aws ec2 describe-instances --region us-east-1 --output table
+aws ec2 describe-instances --query "Reservations[*].Instances[*].{Name: Tags[?Key=='Name'] | [0].Value, Instance_ID: InstanceId, State: State.Name}" --output table
